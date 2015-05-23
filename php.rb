116944e153b16f3341d2f641609a8b8c42b6c7a7
fxbois <fxbois@gmail.com>
2015-05-23 15:56:35 +0200

    remove tidy5

--- php.rb
+++ php.rb
@@ -3,15 +3,15 @@
 require 'formula'
 
 class Php < Formula
-
-# wget "http://fr2.php.net/get/php-5.6.X.tar.bz2/from/this/mirror" -O /Library/Caches/Homebrew/php-5.6.X
+  
+  # wget "http://fr2.php.net/get/php-5.6.X.tar.bz2/from/this/mirror" -O /Library/Caches/Homebrew/php-5.6.X
   url "http://php.net/get/php-5.6.9.tar.bz2/from/this/mirror"
-# openssl sha1 /Library/Caches/Homebrew/php-5.6.X
+  # openssl sha1 /Library/Caches/Homebrew/php-5.6.X
   sha1 "a03f3a168a83a5e6fb983c44933f23fc44323a00"
   homepage "http://php.net"
   version "5.6.9"
-#  revision 1
-
+  # revision 1
+  
   # So PHP extensions don't report missing symbols
   skip_clean 'bin', 'sbin'
   
@@ -22,50 +22,51 @@ class Php < Formula
   depends_on 'libpng'
   depends_on 'libtool'
   depends_on 'mysql'
-  depends_on 'tidy-html5'
+  depends_on 'fxbois/tidy'
+  #depends_on 'tidy-html5'
 
   def install
 
     args = [
       "--prefix=#{prefix}",
       "--localstatedir=#{var}",
-#     "--sysconfdir=#{config_path}",
+      #"--sysconfdir=#{config_path}",
       "--libexecdir=#{libexec}",
       "--mandir=#{man}",
       "--disable-cgi",
       "--disable-debug",
       "--disable-opcache",
-#      "--enable-bcmath",
+      #"--enable-bcmath",
       "--enable-calendar",
-#      "--enable-exif",
-#      "--enable-ftp",
+      #"--enable-exif",
+      #"--enable-ftp",
       "--enable-gd-native-ttf",
-#     "--enable-intl=no",
-#     "--enable-mbregex",
+      #"--enable-intl=no",
+      #"--enable-mbregex",
       "--enable-mbstring",
       "--enable-pcntl",
-#     "--enable-shmop",
+      #"--enable-shmop",
       "--enable-soap",
       "--enable-sockets",
-#      "--enable-sysvmsg",
-#      "--enable-sysvsem",
-#      "--enable-sysvshm",
-#      "--enable-wddx",
+      #"--enable-sysvmsg",
+      #"--enable-sysvsem",
+      #"--enable-sysvshm",
+      #"--enable-wddx",
       "--enable-zip",
       "--with-apxs2=/usr/sbin/apxs",
       "--with-bz2=/usr",
       "--with-config-file-path=/etc",
-#      "--with-config-file-scan-dir=#{config_path}/conf.d",
+      #"--with-config-file-scan-dir=#{config_path}/conf.d",
       "--with-curl",
       "--with-freetype-dir=#{Formula['freetype'].opt_prefix}",
       "--with-gd",
-#      "--with-gettext=#{Formula['gettext'].opt_prefix}",
-#      "--with-iconv-dir=/usr",
-#      "--with-icu-dir=#{Formula['icu4c'].opt_prefix}",
+      #"--with-gettext=#{Formula['gettext'].opt_prefix}",
+      #"--with-iconv-dir=/usr",
+      #"--with-icu-dir=#{Formula['icu4c'].opt_prefix}",
       "--with-jpeg-dir=#{Formula['jpeg'].opt_prefix}",
-#      "--with-kerberos=/usr",
+      #"--with-kerberos=/usr",
       "--with-libedit",
-#      "--with-mhash",
+      #"--with-mhash",
       "--with-mysql-sock=/tmp/mysql.sock",
       "--with-mysql=mysqlnd",
       "--with-mysqli=mysqlnd",
@@ -79,12 +80,11 @@ class Php < Formula
       "--without-pear",
     ]
 
-#    system "./buildconf" if build.head?
     system "./configure", *args
     
     inreplace "Makefile",
-    /^INSTALL_IT = \$\(mkinstalldirs\) '([^']+)' (.+) LIBEXECDIR=([^\s]+) (.+)$/,
-    "INSTALL_IT = $(mkinstalldirs) '#{libexec}/apache2' \\2 LIBEXECDIR='#{libexec}/apache2' \\4"
+              /^INSTALL_IT = \$\(mkinstalldirs\) '([^']+)' (.+) LIBEXECDIR=([^\s]+) (.+)$/,
+              "INSTALL_IT = $(mkinstalldirs) '#{libexec}/apache2' \\2 LIBEXECDIR='#{libexec}/apache2' \\4"
     
     inreplace 'Makefile' do |s|
       s.change_make_var! "EXTRA_LIBS", "\\1 -lstdc++"
@@ -94,11 +94,7 @@ class Php < Formula
     ENV.deparallelize # parallel install fails on some systems
     system "make install"
 
-#    config_path.install default_config => "php.ini" unless File.exist? config_path+"php.ini"
-
     chmod_R 0775, lib+"php"
-
-#    system bin+"pear", "config-set", "php_ini", config_path+"php.ini"
 
   end
 
